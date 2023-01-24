6c6d63c886770e076cfe8f79e25940b167db165a
dabmarcin <dabmarcin@gmail.com>
2023-01-24 22:05:49 +0100

    dzien 4

--- ChallangeApp/ChallangeApp/Program.cs
+++ ChallangeApp/ChallangeApp/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Security.AccessControl;
+
+string imie = "Marcin";
+char plec = 'm';
+int wiek = 40;
+
+if (plec == 'm')
+{
+    if (wiek > 50)
+    {
+        Console.WriteLine("Mezczyzna powyzej 50 roku zycia");
+    }
+    else if (wiek > 38 && imie == "Marcin")
+    {
+        Console.WriteLine("Facet! Po 40 robi sie goraco! ale nie boj nic... Marciny dadza rade :)");
+    }
+}
+ else 
+{
+    Console.WriteLine("Inna osoba!");
+}
+   
